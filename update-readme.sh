6cb0d6788d46943b5a8d49086f2fbfec2e01e360
opsxin <haoxin0880@gmail.com>
2019-09-20 11:03:56 +0800

    Readme: 再更新测试

--- update-readme.sh
+++ update-readme.sh
@@ -3,4 +3,4 @@
 # 不显示 PNG、GIF、WEBP 格式的文件，最大深度为 3 层
 #tree -I "*.png|*.gif|*.webp" -v -L 3 --ignore-case | awk 'BEGIN{print "```bash"} {print} END{print "```"}' > README.md
 #tree -I "*.png|*.gif|*.webp|*.pdf" -v --ignore-case | sed "/md/s|─\ \(.*\)|[\1](https://github.com/opsxin/markdown-notes/blob/master/\1)|" | sed "s/$/\ \ /" > README.md
-find .  -type f -name "*.md" | sed "s|.|https://github.com/opsxin/markdown-notes/blob/master|" | sed "s|.*/\(.*\)|[\1](\0)|" | sed -e "s/\ /%20/" -e "s/$/\ \ /" > README.md
+find .  -type f -name "*.md" | sed "s|.|https://github.com/opsxin/markdown-notes/blob/master|" | sed "s|.*/\(.*\)|[\1](\0)|" | sed -e "s/\ /%20/g" -e "s/$/\ \ /" > README.md
