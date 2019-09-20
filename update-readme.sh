83b2933b2b3bdfdb91a5053ccfcdb7984f60101f
opsxin <haoxin0880@gmail.com>
2019-09-20 13:41:43 +0800

    href 设置为 https://github.com/opsxin/markdown-notes/blob/master

--- update-readme.sh
+++ update-readme.sh
@@ -2,7 +2,7 @@
 
 # 不显示 PNG、GIF、WEBP、PDF 格式的文件，最大深度为 3 层
 #tree -I "*.png|*.gif|*.webp|*.pdf" -v -L 3 --ignore-case | awk 'BEGIN{print "```bash"} {print} END{print "```"}' > README.md
-tree -I "*.png|*.gif|*.webp|*.pdf" -v -L 3 --ignore-case -H / -T Note > README.md
+tree -I "*.png|*.gif|*.webp|*.pdf" -v -L 3 --ignore-case -H 'https://github.com/opsxin/markdown-notes/blob/master' -T markdown-note > README.md
 
 # 这种方式只能正确显示根目录的连接
 # 子目录的连接，因为连接中不包含父目录，所以会造成连接的 404
