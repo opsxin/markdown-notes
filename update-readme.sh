4910b71c6530d9de539ab7e7c6bb48e3e68832b1
opsxin <haoxin0880@gmail.com>
2019-09-20 14:13:30 +0800

    Readme: 最终效果

--- update-readme.sh
+++ update-readme.sh
@@ -2,7 +2,6 @@
 
 # 不显示 PNG、GIF、WEBP、PDF 格式的文件，最大深度为 3 层
 #tree -I "*.png|*.gif|*.webp|*.pdf" -v -L 3 --ignore-case | awk 'BEGIN{print "```bash"} {print} END{print "```"}' > README.md
-tree -I "*.png|*.gif|*.webp|*.pdf" -v -L 3 --ignore-case -H 'https://github.com/opsxin/markdown-notes/blob/master' -T markdown-note > README.md
 
 # 这种方式只能正确显示根目录的连接
 # 子目录的连接，因为连接中不包含父目录，所以会造成连接的 404
@@ -10,3 +9,6 @@ tree -I "*.png|*.gif|*.webp|*.pdf" -v -L 3 --ignore-case -H 'https://github.com/
 
 # 这种方式不能树形显示
 #find .  -type f -name "*.md" | sed "s|.|https://github.com/opsxin/markdown-notes/blob/master|" | sed "s|.*/\(.*\)|[\1](\0)|" | sed "s/$/\ \ /" > README.md
+
+# URL 正确，但是树形会有一些变形
+tree -I "*.png|*.gif|*.webp|*.pdf" -v -L 3 --ignore-case -H 'https://github.com/opsxin/markdown-notes/blob/master' -T markdown-note | awk 'BEGIN{print "<h4>markdown-note</h4>"} /─/{print} /directories/{print}' > README.md
