9259fb92439270fe6dbe403bf2791a2ce6056842
opsxin <haoxin0880@gmail.com>
2019-10-19 17:32:49 +0800

    merge master branch

--- update-readme.sh
+++ update-readme.sh
@@ -2,18 +2,18 @@
 
 # 不显示 PNG、GIF、WEBP、PDF 格式的文件，最大深度为 3 层
 #tree -I "*.png|*.gif|*.webp|*.pdf" -v -L 3 --ignore-case \
-    | awk 'BEGIN{print "```bash"} {print} END{print "```"}' > README.md
+#    | awk 'BEGIN{print "```bash"} {print} END{print "```"}' > README.md
 
 # 能正确显示根目录的连接
 # 子目录因为连接不包含父目录，所以会造成连接的 404
 #tree -I "*.png|*.gif|*.webp|*.pdf" -v --ignore-case \
-    | sed "/md/s|─\ \(.*\)|[\1](https://github.com/opsxin/markdown-notes/blob/master/\1)|" \
-    | sed "s/$/\ \ /" > README.md
+#    | sed "/md/s|─\ \(.*\)|[\1](https://github.com/opsxin/markdown-notes/blob/master/\1)|" \
+#    | sed "s/$/\ \ /" > README.md
 
 # 不能树形显示
 #find . -type f -name "*.md" \
-    | sed "s|.|https://github.com/opsxin/markdown-notes/blob/master|" \
-    | sed "s|.*/\(.*\)|[\1](\0)|" | sed "s/$/\ \ /" > README.md
+#    | sed "s|.|https://github.com/opsxin/markdown-notes/blob/master|" \
+#    | sed "s|.*/\(.*\)|[\1](\0)|" | sed "s/$/\ \ /" > README.md
 
 # URL 正确，但是 PC 端显示会有些变形，Mobile 端正常
 tree -I "*.png|*.gif|*.webp|*.pdf" -v -L 3 --ignore-case \
